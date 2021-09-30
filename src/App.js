ec04e869510e696bc4928bc7adad5193df5cf3d4
metha <matt.ksmp@gmail.com>
2021-09-29 23:59:57 -0700

    create NewForm Component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ import { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [wishlist, setWishlist] = useState([])
+
+  const getWishlist = async () => {
+    try {
+      const wishlist = await fetch("http://localhost:9000/wishlist");
+      const parsedWishlist = await wishlist.json();
+      setWishlist(parsedWishlist);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  useEffect(() => {
+    getWishlist();
+  }, []);
   return (
     <div className="App">
-      
+        <h1>Wishlist App</h1>
     </div>
   );
 }
---
+++ src/components/NewForm.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const NewForm = (props) => {
+  return <h1>New Form</h1>;
+};
+
+export default NewForm;
