a16d711839332e29986f72af3448c9da728a9309
Dae Young Hwang <daeh@daes-air.attlocal.net>
2021-10-01 18:35:33 -0700

    set detail page

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
 
-function ItemDetail() {
+
+function ItemDetail(props) {
+    const currentId = props.match.params.id
+    const [wishlist, setWishlist] = useState({})
+
+    const getWishlist = async(id) => {
+        const foundWishlist = await fetch('http://localhost:9000/wishlist/'+id)
+        const parsedWishlist = await foundWishlist.json()
+        console.log(parsedWishlist)
+        setWishlist(parsedWishlist)
+    };
+
+    useEffect(()=> {
+        getWishlist(currentId)
+    },[currentId]);
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <h1>Detail Page</h1>
+            <p>{wishlist._id}</p>
+            {/* will add code after test */}
+        </>
     )
-}
+};
 
 export default ItemDetail
